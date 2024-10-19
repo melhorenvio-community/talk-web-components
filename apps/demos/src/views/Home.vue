dcf2520323aa47883dbe24c772cfc71fd001ad31
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2024-10-19 08:04:36 -0300

    chore: adding missing @types packages in order to build stencil components

--- apps/demos/src/views/Home.vue
+++ apps/demos/src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <web-hello-world content="I am a Web Component!" />
-  <stencil-first-component></stencil-first-component>
-  <stencil-prop-component name="Juca"></stencil-prop-component>
+
+  <stencil-first-component />
+  <stencil-prop-component name="Juca" />
 </template>

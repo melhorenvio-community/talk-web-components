20271e2165fbf56ac182e6cab2f7b7af3f53f493
Leonardo Adamoli <leonardo.adamoli@melhorenvio.com>
2024-11-04 10:38:33 -0300

    refac: reorganizing home view

--- apps/demos/src/components/app/AppLayout/index.vue
+++ apps/demos/src/components/app/AppLayout/index.vue
@@ -1,49 +1,11 @@
-<script lang="ts" setup>
-import content from "@/static/content.json";
-
-import { ECategoryTypes } from "./types";
-</script>
-
 <template>
   <main class="flex flex-col gap-16 w-full h-full max-w-[800px] py-16 m-auto">
-    <header class="flex flex-col gap-6 items-center text-center">
-      <slot name="heading">
-        <logos-webcomponents class="w-[120px] h-auto" />
-
-        <app-heading>
-          Uma introdução abrangente aos <i>Web Components</i><br> e suas possibilidades
-
-          <template #subheading>
-            <p>
-              Exemplos práticos na construção de <i>Web Components</i> puros,
-              utilizando o <i>framework</i> <b>stencil.js</b> e testando a sua integração com o <b>Vue.js</b>.
-            </p>
-          </template>
-        </app-heading>
-      </slot>
+    <header v-if="$slots.heading" class="flex flex-col gap-6 items-center text-center">
+      <slot name="heading" />
     </header>
 
-    <section class="grid grid-cols-3 gap-5">
-      <slot name="content">
-        <demo-card
-          v-for="({ category, title, description }, index) in content.examples"
-          :key="description"
-        >
-          <template #icon>
-            <logos-webcomponents v-if="category === ECategoryTypes.webComponents" />
-
-            <ion-logo-stencil v-else />
-          </template>
-
-          <template #title>
-            {{ index + 1 }}. {{ title }}
-          </template>
-
-          <template #description>
-            {{ description }}.
-          </template>
-        </demo-card>
-      </slot>
+    <section v-if="$slots.content" class="grid grid-cols-3 gap-5">
+      <slot name="content" />
     </section>
   </main>
 </template>
--- apps/demos/src/views/Home.vue
+++ apps/demos/src/views/Home.vue
@@ -1,3 +1,45 @@
+<script lang="ts" setup>
+import { ECategoryTypes } from "@/components/app/AppLayout/types";
+
+import content from "@/static/content.json";
+</script>
+
 <template>
-  <app-layout />
+  <app-layout>
+    <template #heading>
+      <logos-webcomponents class="w-[120px] h-auto" />
+
+      <app-heading>
+        Uma introdução abrangente aos <i>Web Components</i><br> e suas possibilidades
+
+        <template #subheading>
+          <p>
+            Exemplos práticos na construção de <i>Web Components</i> puros,
+            utilizando o <i>framework</i> <b>stencil.js</b> e testando a sua integração com o <b>Vue.js</b>.
+          </p>
+        </template>
+      </app-heading>
+    </template>
+
+    <template #content>
+      <demo-card
+        v-for="({ category, title, description }, index) in content.examples"
+        :key="description"
+      >
+        <template #icon>
+          <logos-webcomponents v-if="category === ECategoryTypes.webComponents" />
+
+          <ion-logo-stencil v-else />
+        </template>
+
+        <template #title>
+          {{ index + 1 }}. {{ title }}
+        </template>
+
+        <template #description>
+          {{ description }}.
+        </template>
+      </demo-card>
+    </template>
+  </app-layout>
 </template>
